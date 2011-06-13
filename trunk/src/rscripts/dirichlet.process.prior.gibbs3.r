b74bc57a157fc53104e52a1a68aa64557309eada
akaruiws <akaruiws@05815fa0-2b13-9aa7-1478-0cd229ce8aa2>
2011-06-13 11:50:44 +0000

    Ha, one rather rubbish life as it turned out.

--- trunk/src/rscripts/dirichlet.process.prior.gibbs3.r
+++ trunk/src/rscripts/dirichlet.process.prior.gibbs3.r
@@ -1,9 +1,9 @@
-sampleSize<-5
-alpha<-1000000
-t<-c(0.1,0.2,0.7)
+sampleSize<-1
+alpha<-1
+t<-c(0.1,0.2)
 lik<-c(0.2,0.8)
-rec<-vector(length=10000)
-samples<-matrix(nrow=20000,ncol=length(t))
+rec<-vector(length=100000)
+samples<-matrix(nrow=200000,ncol=length(t))
 stepSize<-0.3
 
 for(i in 1:(nrow(samples)/2)){
@@ -49,11 +49,19 @@ for(i in 1:(nrow(samples)/2)){
 	curr.val<-t[update.index]
 	dir<-sample(c(-1,1),1)	
 	proposal<-curr.val+dir*runif(1)*0.3
-	curr.post<-lik[round(curr.val)+1]
+	curr.post<-1
+	for(j in 1:length(t)){
+		curr.post<-curr.post*lik[round(t[j])+1]
+	}
 	if(proposal> 1 || proposal < 0){
 		proposal.post<-0
 	}else{
-		proposal.post<-lik[round(proposal)+1]
+		t2<-t
+		t2[t==curr.val]<-proposal
+		proposal.post = 1
+		for(j in 1:length(t2)){
+			proposal.post<-proposal.post*lik[round(t2[j])+1]
+		}
 	}
 	r<-proposal.post/curr.post
 	s<-runif(1)
@@ -76,13 +84,99 @@ for(i in 1:(nrow(samples)/2)){
 
 hist(samples[,1],prob=T,nclass=2)
 
+mean(samples[,2])
+
+q<-samples[,2]
+
 length(which(samples[,1]>0.5))
 length(which(samples[,1]<0.5))
 
-length(which(samples[,1]))
+
+length(which(samples[,2]>0.5))
+length(which(samples[,2]<0.5))
+
+#length(which(samples[,3]>0.5))
+#length(which(samples[,3]<0.5))
+
+
+
+
 
 cluster.counts<-vector(length=nrow(samples))
 for(i in 1:length(cluster.counts)){
 	cluster.counts[i]<-length(unique(round(samples[i,],5)))
 }
-table(cluster.counts)
+table(cluster.counts[-c(1:20000)])/180000
+
+
+
+###############
+##1 cluster
+p1<-(0.2*0.2+0.8*0.8)*1/2*1/2
+
+##2 cluster
+p2<-(0.2*0.2+0.8*0.8+0.2*0.8*2)*1/4*1/2
+
+ c(p1,p2)/sum(c(p1,p2))
+
+###############
+##1 cluster
+p1<-(0.2*0.2*0.4+0.8*0.8*0.6)*1/2*1/2
+
+##2 cluster
+p2<-(0.2*0.2*0.4*0.4+0.8*0.8*0.6*0.6+0.2*0.8*2*0.4*0.6)*1/4*1/2
+
+1/(0.2*0.2*0.4*0.4*0.25+
+0.8*0.8*0.6*0.6*0.25+
+0.2*0.8*2*0.4*0.6*0.25)
+
+ c(p1,p2)/sum(c(p1,p2))
+
+2.7551*(0.2*0.2*0.4*0.4*1+
+0.8*0.8*0.6*0.6*2+
+0.2*0.8*2*0.4*0.6*3)
+0.2*0.4
+0.8*0.6
+
+0.5^2/2*0.016*5+0.384*5*(1^2-0.5^2)/2
+1/7*(0.25/2)+6/7*(1^2/2-0.5^2/2)
+
+###############
+##1 cluster
+p1<-(0.2^3+0.8^3)*1/2*1/3
+
+##2 cluster
+p2<-(0.2^3+0.8^3+0.2*0.8^2+0.2^2*0.8)*1/4*1/2
+
+##3 cluster
+p3<-(0.2^3+0.8^3+0.2*0.8^2*3+0.2^2*0.8*3)*1/8*1/6
+
+ c(p1,p2,p3)/sum(c(p1,p2,p3))
+sum( c(p1,p2,p3)/sum(c(p1,p2,p3))*c(1:3))
+
+###############
+##1 cluster
+p1.1<-(0.2^3)*0.4
+p1.2<-(0.8^3)*0.6
+p1<-sum(c(p1.1,p1.2)*1/3)
+
+##2 cluster
+p2.1<-0.2^3*0.4*0.4
+p2.2<-0.8^3*0.6*0.6
+p2.3<-0.2^2*0.8*0.4*0.6
+p2.4<-0.8^2*0.2*0.4*0.6
+p2<-sum(c(p2.1,p2.2,p2.3,p2.4)*1/2)
+
+##3 cluster
+p3.1<-0.2^3*0.4^3
+p3.2<-0.8^3*0.6^3
+p3.3<-0.2*0.8^2*0.4*0.6^2*3
+p3.4<-0.2^2*0.8*0.4^2*0.6*3
+p3<-sum(c(p3.1,p3.2,p3.3,p3.4)*1/6)
+
+p<-c(p1,p2,p3)/sum(c(p1,p2,p3))
+
+sum(p*c(1:3))
+
+
+
