6b738eccf6cfc29a896243e5eb0adc2c49122d57
akaruiws <akaruiws@05815fa0-2b13-9aa7-1478-0cd229ce8aa2>
2012-05-30 17:22:15 +0000

    Test commit.

---
+++ trunk/src/TestLibrary1.java
@@ -0,0 +1,13 @@
+import beast.math.matrixAlgebra.RobustSingularValueDecomposition;
+import cern.colt.matrix.DoubleMatrix2D;
+
+/**
+ * @author Chieh-Hsi Wu
+ */
+public class TestLibrary1 {
+    public static void main(String[] args){
+        RobustSingularValueDecomposition svd;
+        DoubleMatrix2D eigenV;
+
+    }
+}
