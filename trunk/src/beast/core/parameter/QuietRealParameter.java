91487b9f93a4856581982b5da0565c8580c57c75
akaruiws <akaruiws@05815fa0-2b13-9aa7-1478-0cd229ce8aa2>
2012-08-21 05:26:54 +0000

    Test commit.

--- trunk/src/beast/core/parameter/QuietRealParameter.java
+++ trunk/src/beast/core/parameter/QuietRealParameter.java
@@ -7,6 +7,7 @@ import beast.core.Description;
  */
 @Description("A real parameter class that allows values to be set quietly. It's convenience is beyond imagination.")
 public class QuietRealParameter extends RealParameter{
+    private int idNumber = -1;
     public QuietRealParameter(){
 
     }
@@ -26,4 +27,27 @@ public class QuietRealParameter extends RealParameter{
         m_bIsDirty[dim] = true;
         m_nLastDirty = dim;
     }
+
+    public void setIDNumber(int idNumber){
+        this.idNumber = idNumber;
+    }
+
+    public int getIDNumber(){
+        return idNumber;
+    }
+
+    @Override
+    public QuietRealParameter copy() {
+        try {
+            @SuppressWarnings("unchecked") final
+            QuietRealParameter copy = (QuietRealParameter) this.clone();
+            copy.values = values.clone();//new Boolean[values.length];
+            copy.m_bIsDirty = new boolean[values.length];
+            return copy;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
