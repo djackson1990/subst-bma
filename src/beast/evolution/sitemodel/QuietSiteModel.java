ee62b4f5f4230da163c1b342586f42f3147357e0
akaruiws <akaruiws@05815fa0-2b13-9aa7-1478-0cd229ce8aa2>
2012-08-29 03:11:35 +0000

    Multivariate related.

---
+++ src/beast/evolution/sitemodel/QuietSiteModel.java
@@ -0,0 +1,14 @@
+package beast.evolution.sitemodel;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: cwu080
+ * Date: 29/08/12
+ * Time: 2:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class QuietSiteModel extends SiteModel {
+    public void makeAccept(){
+        super.accept();
+    }
+}
