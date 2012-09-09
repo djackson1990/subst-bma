cdf866663d41caff3666fd924da941df781dc376
akaruiws <akaruiws@05815fa0-2b13-9aa7-1478-0cd229ce8aa2>
2012-09-09 11:31:10 +0000

    More operator stuff.

---
+++ trunk/src/beast/evolution/operators/NtdBMAPointersSwapOperator.java
@@ -0,0 +1,76 @@
+package beast.evolution.operators;
+
+import beast.core.Input;
+import beast.core.Operator;
+import beast.core.parameter.DPPointer;
+import beast.core.parameter.DPValuable;
+import beast.evolution.alignment.Alignment;
+import beast.util.Randomizer;
+
+/**
+ * @author Chieh-Hsi Wu
+ */
+public class NtdBMAPointersSwapOperator extends Operator {
+    //assignment
+    public Input<DPPointer> paramPointersInput = new Input<DPPointer>(
+            "paramPointers",
+            "array which points a set of unique parameter values",
+            Input.Validate.REQUIRED
+    );
+
+    public Input<DPPointer> modelPointersInput = new Input<DPPointer>(
+            "modelPointers",
+            "array which points a set of unique model",
+            Input.Validate.REQUIRED
+    );
+
+    public Input<DPPointer> freqPointersInput = new Input<DPPointer>(
+            "freqPointers",
+            "array which points a set of unique model",
+            Input.Validate.REQUIRED
+    );
+    public Input<DPValuable> dpValInput = new Input<DPValuable>("dpVal", "A valuable that stores information about the category assignments", Input.Validate.REQUIRED);
+    public Input<Alignment> alignmentInput = new Input<Alignment>("alignment", "A valuable that stores information about the category assignments", Input.Validate.REQUIRED);
+
+    private DPValuable dpVal;
+    private Alignment alignment;
+
+    public void initAndValidate(){
+        dpVal = dpValInput.get();
+        alignment = alignmentInput.get();
+    }
+
+    public double proposal(){
+        int categoryCount = dpVal.getCategoryCount();
+        if(categoryCount == 1){
+            return Double.NEGATIVE_INFINITY;
+        }
+
+        int categoryIndex1 = (int)(Randomizer.nextDouble()*categoryCount);
+        int categoryIndex2 = categoryIndex1;
+        while(categoryIndex1 == categoryIndex2){
+            int something = (int)(Randomizer.nextDouble()*categoryCount);
+            //System.out.println("Hi! "+categoryCount+" "+something);
+            categoryIndex2 = something;
+        }
+
+        int[] sitesInCategoryIndex1 = dpVal.getClusterSites(categoryIndex1);
+        int[] sitesInCategoryIndex2 = dpVal.getClusterSites(categoryIndex2);
+        int site1 = sitesInCategoryIndex1[(int)(sitesInCategoryIndex1.length*Randomizer.nextDouble())];
+        int site2 = sitesInCategoryIndex2[(int)(sitesInCategoryIndex2.length*Randomizer.nextDouble())];
+        if(alignment.getPatternIndex(site1) == alignment.getPatternIndex(site2)){
+            return Double.NEGATIVE_INFINITY;
+        }
+
+
+        DPPointer paramPointers = paramPointersInput.get(this);
+        paramPointers.swapPointers(site1, site2);
+        DPPointer modelPointers = modelPointersInput.get(this);
+        modelPointers.swapPointers(site1, site2);
+        DPPointer freqPointers = freqPointersInput.get(this);
+        freqPointers.swapPointers(site1, site2);
+
+        return 0.0;
+    }
+
+}
