39959b43291f3be9dd4769d5a05754a09320f895
akaruiws <akaruiws@05815fa0-2b13-9aa7-1478-0cd229ce8aa2>
2012-11-16 01:44:31 +0000

    Description added to the rest of the classes.

--- trunk/src/beast/core/Recycle.java
+++ trunk/src/beast/core/Recycle.java
@@ -1,12 +1,9 @@
 package beast.core;
 
 /**
- * Created by IntelliJ IDEA.
- * User: cwu080
- * Date: 1/09/2011
- * Time: 2:37:18 PM
- * To change this template use File | Settings | File Templates.
+ * @author Chieh-Hsi Wu
  */
+@Description("Derived classes will reuse objects that have been removed from the list.")
 public interface Recycle {
     public int getObjectCreatedCount();
 }
