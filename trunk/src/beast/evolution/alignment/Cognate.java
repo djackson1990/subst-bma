6bf4352201e0bc9df5e7bb65fb36d934c4bcea04
akaruiws <akaruiws@05815fa0-2b13-9aa7-1478-0cd229ce8aa2>
2012-11-17 07:17:38 +0000

    Components for simon's data.

---
+++ trunk/src/beast/evolution/alignment/Cognate.java
@@ -0,0 +1,40 @@
+package beast.evolution.alignment;
+
+import beast.core.Input;
+import beast.core.Plugin;
+import beast.evolution.datatype.DataType;
+import beast.evolution.datatype.LanguageIntegerData;
+
+/**
+ * @author Chieh-Hsi Wu
+ */
+public class Cognate extends Plugin {
+
+    public Input<String> wordCodeInput = new Input<String>("words","Words coded as digits separated by commas", Input.Validate.REQUIRED);
+
+    private String[] words;
+    public void initAndValidate(){
+        words = wordCodeInput.get().split("\\s+");
+    }
+
+    public int getWordCount(){
+        return words.length;
+    }
+
+    public int[] getWords(DataType dataType){
+        if(!(dataType instanceof LanguageIntegerData)){
+            throw new RuntimeException("Require LanguageIntegerData.");
+        }
+        int[] wordCodes = new int[words.length];
+        for(int i = 0; i < wordCodes.length; i++){
+            wordCodes[i] = ((LanguageIntegerData)dataType).string2code(words[i]);
+        }
+        return wordCodes;
+
+    }
+
+
+
+
+
+}
